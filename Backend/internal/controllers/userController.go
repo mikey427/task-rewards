2f607f61ece185c7f603e4249f073be8c7968b1d
Michael <mrbaez427@gmail.com>
2025-07-26 13:49:48 -0700

    Signup working

--- Backend/internal/controllers/userController.go
+++ Backend/internal/controllers/userController.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mikey427/Backend/internal/database"
+	"github.com/mikey427/Backend/internal/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Signup(c *gin.Context) {
@@ -23,8 +26,58 @@ func Signup(c *gin.Context) {
 	}
 
 	// Hash the password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password from body",
+		})
+
+		return
+	}
 
 	// Create the user
+	user := models.User{Email: body.Email, Password: string(hash)}
+
+	result := database.DB.Create(&user)
+
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create user",
+		})
+
+		return
+	}
 
 	// Respond
+
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+func Login(c *gin.Context) {
+
+	// Get the email and password off req body
+	var body struct {
+		Email    string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+
+	}
+
+	// Look up requested user
+	result := database.DB.Where("name = ?", "jinzhu").First(&user)
+
+	// Compare sent in password with saved user pass hash
+
+	// Generate a JWT token
+
+	// Send it back
+
 }
--- Backend/main.go
+++ Backend/main.go
@@ -5,22 +5,19 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/mikey427/Backend/internal/config"
+	"github.com/mikey427/Backend/internal/controllers"
 	"github.com/mikey427/Backend/internal/database"
 )
 
 func init() {
 	config.LoadEnvVariables()
 	database.InitConnection()
-	database.SyncDatabase()
+	// database.SyncDatabase()
 }
 
 func main() {
 	fmt.Println("Hello")
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router.POST("/signup", controllers.Signup)
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
