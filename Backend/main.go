73a6ba86eb75d7c147125a3a3cd6445dab90c34e
Michael <mrbaez427@gmail.com>
2025-07-31 23:58:11 -0700

    Found the bug..

--- Backend/main.go
+++ Backend/main.go
@@ -34,7 +34,7 @@ func main() {
 	router.GET("/api/chores", middleware.RequireAuth, controllers.RetrieveAllChores)
 	router.POST("/api/create-chore", middleware.RequireAuth, controllers.CreateChore)
 	router.DELETE("/api/delete-chore/:ID", middleware.RequireAuth, controllers.DeleteChore)
-	router.PUT("/api/update-chore", middleware.RequireAuth, controllers.Update)
+	router.PUT("/api/update-chore", middleware.RequireAuth, controllers.UpdateChore)
 	router.GET("/api/shop", middleware.RequireAuth, controllers.RetrieveShop)
 	router.POST("/api/create-shop-item", middleware.RequireAuth, controllers.CreateShopItem)
 	router.DELETE("/api/delete-shop-item/:ID", middleware.RequireAuth, controllers.DeleteShopItem)
